f14927c7f850158d254958fcca8653cf78573f7f
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2026-04-13 23:40:53 +0400

    feat(character): create Blaster character C++ and Blueprint classes

---
+++ Source/Blaster/Private/BlasterGameModeBase.cpp
@@ -0,0 +1,2 @@
+#include "BlasterGameModeBase.h"
+
---
+++ Source/Blaster/Public/Character/BlasterCharacter.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BlasterCharacter.generated.h"
+
+UCLASS()
+class BLASTER_API ABlasterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABlasterCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
