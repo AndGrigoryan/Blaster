7d49ad5861c83b2d2200b45edfef355e683a5e7b
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2026-04-17 13:51:39 +0400

    feat(weapon): set up weapon components and create WeaponState enum

--- Source/Blaster/Private/Weapon/WeaponBase.cpp
+++ Source/Blaster/Private/Weapon/WeaponBase.cpp
@@ -1,15 +1,37 @@
 #include "Weapon/WeaponBase.h"
 
+#include "Components/SphereComponent.h"
+
+
 AWeaponBase::AWeaponBase()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	SetRootComponent(WeaponMesh);
+
+	WeaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	WeaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	AreaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreaSphere"));
+	AreaSphere->SetupAttachment(RootComponent);
+	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 
 }
 
 void AWeaponBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (HasAuthority())
+	{
+		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	}
 }
 
 void AWeaponBase::Tick(float DeltaTime)
--- Source/Blaster/Public/Weapon/WeaponBase.h
+++ Source/Blaster/Public/Weapon/WeaponBase.h
@@ -4,6 +4,17 @@
 #include "GameFramework/Actor.h"
 #include "WeaponBase.generated.h"
 
+UENUM()
+enum class EWeaponState : uint8
+{
+	EWS_Initial UMETA(DisplayName = "Initial state"),
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+
+	EWS_MAX UMETA(DisplayName = "DefaultMAX"),
+
+};
+
 UCLASS()
 class BLASTER_API AWeaponBase : public AActor
 {
@@ -17,5 +28,15 @@ protected:
 
 public:	
 	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	TObjectPtr<class USphereComponent> AreaSphere;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	EWeaponState WeaponState;
 
 };
