73c421b309b828027a47fb24dfb7aa4bd5bffb1c
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2026-06-16 17:21:26 +0400

    feat(weapon): create ProjectileWeapon class

---
+++ Source/Blaster/Public/Weapon/ProjectileWeapon.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/WeaponBase.h"
+#include "ProjectileWeapon.generated.h"
+
+
+UCLASS()
+class BLASTER_API AProjectileWeapon : public AWeaponBase
+{
+	GENERATED_BODY()
+	
+};
