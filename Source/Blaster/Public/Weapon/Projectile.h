dd50c7112477d29c0bf8f02e66c903d318e1ada4
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2026-06-16 17:21:42 +0400

    feat(weapon): create Projectile class and setup collision

---
+++ Source/Blaster/Public/Weapon/Projectile.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class BLASTER_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UBoxComponent> CollisionBox;
+};
